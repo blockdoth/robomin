31eac054655bf2bf3b94db4affba1187fa4b6864
blockdoth <pepijn.pve@gmail.com>
2025-09-01 14:51:11 +0200

    rust hello world

--- flake.nix
+++ flake.nix
@@ -1,32 +1,52 @@
 {
-
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs =
-    { self, nixpkgs }:
-    let
-      supportedSystems = [
+    inputs@{ self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
         "x86_64-linux"
         "aarch64-linux"
         "x86_64-darwin"
         "aarch64-darwin"
       ];
-      forEachSupportedSystem =
-        f: nixpkgs.lib.genAttrs supportedSystems (system: f { pkgs = import nixpkgs { inherit system; }; });
-    in
-    {
-      devShells = forEachSupportedSystem (
-        { pkgs }:
+      perSystem =
         {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-
-            ];
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [ inputs.rust-overlay.overlays.default ];
           };
-        }
-      );
+          toolchain = pkgs.rust-bin.fromRustupToolchainFile ./toolchain.toml;
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              toolchain
+              rust-analyzer-unwrapped
+              mprocs
+            ];
+            RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
+          };
+
+          packages.default = pkgs.rustPlatform.buildRustPackage {
+            pname = "pathfinding";
+            version = "0.0.1";
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+            cargoToml = ./Cargo.toml;
+            release = true;
+            nativeBuildInputs = [ toolchain ];
+          };
+        };
     };
 }
-
